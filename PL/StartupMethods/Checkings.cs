eec1712c1e34464468e09ded5527cb9c288d1427
m00nler <91968598+m00nler@users.noreply.github.com>
2022-06-23 11:49:00 +0300

    Did startup method better

---
+++ PL/StartupMethods/Checkings.cs
@@ -0,0 +1,77 @@
+﻿using BLL.Services;
+using Core.Models;
+using Core.NewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.StartupMethods
+{
+    public class Checkings
+    {
+        public Direction FrameTick(Direction currentDirection, Core.NewModels.Player player, Map map)
+        {
+            var (dx, dy) = DirectionsDictionary.directions[currentDirection];
+            foreach (var item in map.map)
+            {
+                if (player.X + dx == item.X && player.Y + dy == item.Y && (item is Wall || item is EnergyBall))
+                {
+                    return Direction.Stop;
+                }
+                else if (player.X == 0 || player.Y == 0 || player.X + 1 == map.map.GetLength(0) || player.Y + 1 == map.map.GetLength(1))
+                {
+                    return ChangeDirection(currentDirection);
+                }
+            }
+            return currentDirection;
+        }
+
+        public Direction FrameTickBall(Direction dir, Core.NewModels.Ball ball, Map map, Core.NewModels.Player player, ref int _score)
+        {
+            var (dx, dy) = DirectionsDictionary.directions[dir];
+            if (ball.X + 1 == map.map.GetLength(0) || ball.Y + 1 == map.map.GetLength(1) || ball.X == 0 || ball.Y == 0)
+            {
+                return ChangeDirection(dir);
+            }
+            foreach (var item in map.map)
+            {
+                if (ball.X + dx == item.X && ball.Y + dy == item.Y && (item is EnergyBall))
+                {
+                    _score++;
+                    return dir;
+                }
+
+                if (ball.X + dx == item.X && ball.Y + dy == item.Y && (item is Wall))
+                {
+                    return ChangeDirection(dir);
+                }
+                if (ball.X + dx == item.X && ball.Y + dy == item.Y && (item is Core.NewModels.Player))
+                {
+                    return ChangeDirectionWithPlayer(dir, player);
+                }
+            }
+            return dir;
+        }
+
+        public Direction ChangeDirectionWithPlayer(Direction dir, Core.NewModels.Player player)
+        {
+
+            var vertical = dir == Direction.Up || dir == Direction.Down;
+            int delta;
+            delta = vertical ? 1 : -1;
+            if (player.reverseSlash)
+            {
+                delta *= -1;
+            }
+
+            return (Direction)(((int)dir + delta) % 4);
+        }
+
+        public Direction ChangeDirection(Direction dir)
+        {
+            return (Direction)(((int)dir + 2) % 4);
+        }
+    }
+}
--- PL/StartupMethods/Startup.cs
+++ PL/StartupMethods/Startup.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using BLL.Services;
-using System.Linq;
 using BLL.Interfaces;
 using Core.Models;
 using Core.NewModels;
@@ -15,35 +13,31 @@ namespace PL.StartupMethods
     public class Startup
     {
         private readonly IUserService _userService;
-        public static Dictionary<Direction, (int,int)> directions = new Dictionary<Direction, (int,int)>();
-        Core.NewModels.Player player;
-        Core.NewModels.Ball ball;
+        Core.NewModels.Player player = new Core.NewModels.Player(0,0);
+        Core.NewModels.Ball ball = new Core.NewModels.Ball(0,0);
         private int _score = 0;
         private int _total = 0;
         public Startup(IUserService service)
         {
             _userService = service;
         }
-
-        private const int _mapWidth = 60;
-        private const int _mapHeight = 40;
-
         private int _frameRate = 50;
 
         private readonly Map map= new Map();
-        private readonly DrawMenu _drawMenu = new DrawMenu();
+        private readonly Checkings _check = new Checkings();
 
         public async Task StartGame()
         {
-            directions.Add(Direction.Right, (1, 0));
-            directions.Add(Direction.Left, (-1, 0));
-            directions.Add(Direction.Up, (0, -1));
-            directions.Add(Direction.Down, (0, 1));
-            directions.Add(Direction.Stop, (0, 0));
             Console.SetWindowSize(150, 100);
             Console.SetBufferSize(150, 100);
             Console.SetWindowSize(100, 60);
             Console.SetBufferSize(100, 60);
+            Direction currentDirection = Direction.Right;
+            Direction currentDirectionPlayer = Direction.Stop;
+            var oldMovementPlayer = currentDirectionPlayer;
+            var movement = new Movement();
+            var time = new TimeCheck();
+            bool changeWall = false;
             var menu = new DrawMainMenu();
             menu.DrawMenu();
 
@@ -52,32 +46,10 @@ namespace PL.StartupMethods
             {
                 var user = await login.Login(_userService);
                 map.CreateMap();
-                foreach (var i in map.map)
-                {
-                    if (i is Core.NewModels.Player)
-                    {
-                        player = (Core.NewModels.Player)i;
-                    }
-                    else if (i is Core.NewModels.Ball)
-                    {
-                        ball = (Core.NewModels.Ball)i;
-                    }
-                    else if (i is EnergyBall)
-                    {
-                        _total++;
-                    }
-                }
+                DetermineElements(ref player, ref ball);
 
                 Console.ReadKey();
                 Console.CursorVisible = false;
-                var timeWatch = new TimeCheck();
-                Direction currentDirection = Direction.Right;
-                Direction currentDirectionPlayer = Direction.Stop;
-                var oldMovementPlayer = currentDirectionPlayer;
-                var movement = new Movement();
-                var time = new TimeCheck();
-                bool changeWall = false;
-                var count = 0;
                 time.StartTimeChecking();
                 while (true)
                 {
@@ -92,8 +64,8 @@ namespace PL.StartupMethods
                             movement.ProcessKey(ref currentDirectionPlayer, ref changeWall);
                         }
                     }
-                    currentDirection = FrameTickBall(currentDirection, ball);
-                    currentDirectionPlayer = FrameTick(currentDirectionPlayer, player);
+                    currentDirection = _check.FrameTickBall(currentDirection, ball, map, player, ref _score);
+                    currentDirectionPlayer = _check.FrameTick(currentDirectionPlayer, player,map);
                     if (currentDirectionPlayer != Direction.Stop)
                     {
                         player.Move(currentDirectionPlayer);
@@ -138,69 +110,23 @@ namespace PL.StartupMethods
                 Console.WriteLine("Bruh, learn how to read");
             }
         }
-
-        private Direction FrameTick(Direction currentDirection, Core.NewModels.Player player)
+        private void DetermineElements(ref Core.NewModels.Player player, ref Core.NewModels.Ball ball)
         {
-            var (dx, dy) = directions[currentDirection];
-            foreach (var item in map.map)
+            foreach (var i in map.map)
             {
-                if(player.X + dx == item.X && player.Y + dy == item.Y &&(item is Wall || item is EnergyBall))
+                if (i is Core.NewModels.Player)
                 {
-                    return Direction.Stop ;
+                    player = (Core.NewModels.Player)i;
                 }
-                else if (player.X == 0 || player.Y == 0 || player.X + 1== map.map.GetLength(0) || player.Y + 1 == map.map.GetLength(1))
+                else if (i is Core.NewModels.Ball)
                 {
-                    return ChangeDirection(currentDirection);
+                    ball = (Core.NewModels.Ball)i;
+                }
+                else if (i is EnergyBall)
+                {
+                    _total++;
                 }
             }
-            return currentDirection;
-        }
-
-        private Direction FrameTickBall(Direction dir, Core.NewModels.Ball ball)
-        {
-            var (dx, dy) = directions[dir];
-            if (ball.X + 1 == map.map.GetLength(0) || ball.Y + 1 == map.map.GetLength(1) || ball.X == 0  || ball.Y == 0)
-            {
-                return ChangeDirection(dir);
-            }
-            foreach (var item in map.map)
-            {
-                if (ball.X + dx == item.X && ball.Y + dy == item.Y && (item is EnergyBall))
-                {
-                    _score++;
-                    return dir;
-                }
-
-                if (ball.X + dx == item.X && ball.Y + dy == item.Y && (item is Wall))
-                {
-                    return ChangeDirection(dir);
-                }
-                if(ball.X + dx == item.X && ball.Y + dy == item.Y && (item is Core.NewModels.Player))
-                {
-                    return ChangeDirectionWithPlayer(dir, player);
-                }
-            }
-            return dir;
-        }
-
-        private Direction ChangeDirectionWithPlayer(Direction dir, Core.NewModels.Player player)
-        {
-
-            var vertical = dir == Direction.Up || dir == Direction.Down;
-            int delta;
-            delta = vertical ? 1 : -1;
-            if (player.reverseSlash)
-            {
-                delta *= -1;
-            }
-
-            return (Direction)(((int)dir + delta) % 4);
-        }
-
-        private Direction ChangeDirection(Direction dir)
-        {
-            return (Direction)(((int)dir + 2) % 4);
         }
     }
-
 }
