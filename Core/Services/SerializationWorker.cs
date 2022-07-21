6e1d9def270f3acc692a6008e68735a07038c889
m00nler <91968598+m00nler@users.noreply.github.com>
2022-07-21 21:15:15 +0300

    Added setting to serializer for better view

--- Core/Services/SerializationWorker.cs
+++ Core/Services/SerializationWorker.cs
@@ -7,18 +7,22 @@ namespace DAL.Services
 {
     class SerializationWorker : ISerializationWorker
     {
-        JsonSerializerSettings settings = new() { TypeNameHandling = TypeNameHandling.Auto };
+        JsonSerializerSettings _settings = new() 
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            Formatting = Formatting.Indented,
+        };
 
         public void Serialize<TEntity>(TEntity obj, string jsonFileName)
         {
-            var json = JsonConvert.SerializeObject(obj, settings);
+            var json = JsonConvert.SerializeObject(obj, _settings);
             File.WriteAllText(jsonFileName, json);
         }
 
         public TEntity Deserialize<TEntity>(string fileName)
         {
             var json = File.ReadAllText(fileName);
-            return JsonConvert.DeserializeObject<TEntity>(json, settings);
+            return JsonConvert.DeserializeObject<TEntity>(json, _settings);
         }
     }
 }
--- DAL/Services/SerializationWorker.cs
+++ DAL/Services/SerializationWorker.cs
@@ -7,16 +7,21 @@ namespace Core.Services
 {
     class SerializationWorker : ISerializationWorker
     {
+        private readonly JsonSerializerSettings _settings = new()
+        {
+            Formatting = Formatting.Indented
+        };
+
         public void Serialize<TEntity>(TEntity obj, string jsonFileName)
         {
-            var json = JsonConvert.SerializeObject(obj);
+            var json = JsonConvert.SerializeObject(obj, _settings);
             File.WriteAllText(jsonFileName, json);
         }
 
         public TEntity Deserialize<TEntity>(string fileName)
         {
             var json = File.ReadAllText(fileName);
-            return JsonConvert.DeserializeObject<TEntity>(json);
+            return JsonConvert.DeserializeObject<TEntity>(json, _settings);
         }
     }
 }
